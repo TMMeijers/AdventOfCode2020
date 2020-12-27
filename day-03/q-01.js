4f7f3d97b2cdb079fa3093a740b567162b79a0a1
TMMeijers <t.m.meijers@protonmail.com>
2020-12-27 19:48:01 +0100

    day 03 solved

---
+++ day-03/q-01.js
@@ -0,0 +1,22 @@
+const files = require('../utils/files.js')
+const path = require('path')
+
+const STEP_RIGHT = 3
+const STEP_DOWN = 1
+
+async function main() {
+  const rowsOfTrees = await files.readLines(path.resolve(__dirname + '/input.txt'))
+  const width = rowsOfTrees[0].length
+
+  let x = 0
+  let treesHit = 0
+
+  for (let y = 0; y < rowsOfTrees.length; y += STEP_DOWN) {
+    treesHit += rowsOfTrees[y].charAt(x) === '#'
+    x = (x + STEP_RIGHT) % width
+  }
+
+  console.warn(`Amount of trees hit on the way down: ${treesHit}`)
+}
+
+main()
---
+++ day-03/q-02.js
@@ -0,0 +1,32 @@
+const files = require('../utils/files.js')
+const path = require('path')
+
+async function main() {
+  const angles = [
+    { right: 1, down: 1 },
+    { right: 3, down: 1 },
+    { right: 5, down: 1 },
+    { right: 7, down: 1 },
+    { right: 1, down: 2 },
+  ]
+  const rowsOfTrees = await files.readLines(path.resolve(__dirname + '/input.txt'))
+  const width = rowsOfTrees[0].length
+
+  const treeHits = angles.map(({ right, down }) => {
+    let x = 0
+    let treesHit = 0
+
+    for (let y = 0; y < rowsOfTrees.length; y += down) {
+      treesHit += rowsOfTrees[y].charAt(x) === '#'
+      x = (x + right) % width
+    }
+
+    return treesHit
+  })
+
+  console.warn(`Amount of trees hit on the way down: ${treeHits.join(', ')}`)
+  const product = treeHits.reduce((product, hit) => product * hit, 1)
+  console.warn(`Product of tree hits: ${product}`)
+}
+
+main()
