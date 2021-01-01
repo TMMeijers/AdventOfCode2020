c27a1c0012e12e210a762906c05c78ea246ba043
TMMeijers <t.m.meijers@protonmail.com>
2021-01-01 18:57:36 +0100

    day 08 q02 solved

--- day-06/q02.js
+++ day-06/q02.js
@@ -5,11 +5,22 @@ async function main() {
   const input = await files.readFileP(path.resolve(__dirname, 'input.txt'))
 
   const groups = input.split('\n\n').map((group) => {
-    return group.split('\n').filter((person) => person !== '\n')
+    return group.split('\n').filter((person) => person !== '\n' && person !== '')
   })
 
+  function getConsistentAnswers(groupAnswers) {
+    const possibleAnswers = Object.keys(groupAnswers.reduce((map, personAnswers) => {
+      personAnswers.split('').forEach((answer) => map[answer] = true)
+      return map
+    }, {}))
+
+    return possibleAnswers.reduce((consistent, answer) => {
+      return consistent + groupAnswers.every((personAnswers) => personAnswers.indexOf(answer) >= 0)
+    }, 0)
+  }
+
   const sum = groups.reduce((sum, group) => {
-    return sum + Object.keys(group).length
+    return sum + getConsistentAnswers(group)
   }, 0)
 
   console.warn(`The sum of all group's answers is: ${sum}`)
